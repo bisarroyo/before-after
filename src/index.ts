e48f71120e69da73da14103d0f7ac325b9a7148b
Bismark Arroyo <bis.arroyo@gmail.com>
2023-10-31 19:44:25 -0600

    add index and selectors html

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const beforeImage = document.getElementById(
+  'before-image'
+) as HTMLImageElement | null
+const afterImage = document.getElementById(
+  'after-image'
+) as HTMLImageElement | null
