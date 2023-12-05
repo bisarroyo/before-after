17991968925b8a4545812f7d9d1a259628b78cd6
Bismark Arroyo <bis.arroyo@gmail.com>
2023-12-04 21:09:02 -0600

    remove init example

--- src/index.ts
+++ src/index.ts
@@ -100,9 +100,9 @@ class BeforeAfterEffect {
 }
 
 // Ejemplo de uso
-const instance = new BeforeAfterEffect('btaContainer', {
-  width: '500px',
-  height: '300px',
-  rangeInitialValue: '10',
-  rangeInitialColor: '#000000'
-})
+// const instance = new BeforeAfterEffect('btaContainer', {
+//   width: '500px',
+//   height: '300px',
+//   rangeInitialValue: '10',
+//   rangeInitialColor: '#000000'
+// })
